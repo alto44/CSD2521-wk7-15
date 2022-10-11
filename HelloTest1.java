b7263cd2f33310c5fba92128cd02f54361780ad7
0528701 <111748978+0528701@users.noreply.github.com>
2022-10-10 22:59:54 -0400

    Update HelloTest1.java
    I added an update on line 3 and modified the comments on line 8

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,10 +1,11 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/10/2022 CRT
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
-    /*
+    /* this is my first pull request
      * Replace your comments here!!
      */
     public static void main(String[] args) {
